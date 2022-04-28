ae652f8d2582ffe702297ea5b480baf514204242
Renan Fortunato <renan@clinicadoleite.com.br>
2022-04-28 13:14:42 -0300

    create entity User

---
+++ src/Domain/Entities/User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppFinance\Domain\Entities;
+
+use AppFinance\Domain\Enums\UserType;
+use AppFinance\Domain\ValueObjects\Email;
+use AppFinance\Shared\Entity;
+
+class User extends Entity
+{
+    private Email $email;
+    private string $name;
+    private string $user_type;
+    private string $hashed_password;
+
+    private function __construct(
+        $id,
+        Email $email,
+        string $name,
+        string $user_type,
+        string $hashed_password
+    )
+    {
+        parent::__construct($id);
+        $this->name = $name;
+        $this->email = $email;
+        $this->user_type = $user_type;
+        $this->hashed_password = $hashed_password;
+
+        $this->validate();
+    }
+
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getUserType(): string
+    {
+        return $this->user_type;
+    }
+
+    public function getHashedPassword(): string
+    {
+        return $this->hashed_password;
+    }
+
+    private function validate()
+    {
+        if (! in_array($this->user_level, UserType::getValues())  ) {
+            throw new \DomainException("Tipo de usuário é inválido.");
+        }
+        if (empty($this->hashed_password)) {
+            throw new \DomainException("Senha do usuário não pode estar vazia.");
+        }
+    }
+}
---
+++ src/Domain/Enums/UserType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppFinance\Domain\Enums;
+
+use AppFinance\Shared\Enum;
+
+class UserType extends Enum
+{
+    const COMMON = 'COMMON';
+    const STORE = 'STORE';
+}
