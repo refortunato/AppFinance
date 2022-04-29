44809b756e87f4ffd054288334fce4f3813af305
Renan Fortunato <renanfortunato09@gmail.com>
2022-04-29 07:25:24 -0300

    change transfer run date to now

--- src/Domain/Entities/Transfer.php
+++ src/Domain/Entities/Transfer.php
@@ -7,24 +7,22 @@ use AppFinance\Shared\Entity;
 class Transfer extends Entity
 {
     private \DateTime $run_date;
-    private CommonUser $origin;
-    private User $destiny;
+    private CommonUser $origin_account;
+    private User $destiny_account;
     private float $value;
 
     public function __construct(
         $id,
-        \DateTime $rud_date,
-        CommonUser $origin,
-        User $destiny,
+        CommonUser $origin_account,
+        User $destiny_account,
         float $value
     )
     {
         parent::__construct($id);  
-        $this->rud_date = $rud_date;
-        $this->origin = $origin;
-        $this->destiny = $destiny;
-        $this->value = $value;
-
+        $this->run_date = new \DateTime('now', new \DateTimeZone('UTC'));
+        $this->origin_account = $origin_account;
+        $this->destiny_account = $destiny_account;
+        $this->value = round($value, 2);
         $this->validate();
     }
 
@@ -33,14 +31,14 @@ class Transfer extends Entity
         return $this->run_date;
     }
 
-    public function getOrigin (): User 
+    public function getOriginAccount (): User 
     { 
-        return $this->origin;
+        return $this->origin_account;
     }
 
-    public function getDestiny(): User
+    public function getDestinyAccount(): User
     { 
-        return $this->destiny;
+        return $this->destiny_account;
     }
     public function getValue(): float
     { 
@@ -51,6 +49,10 @@ class Transfer extends Entity
     {
         if ($this->value <= 0) {
             throw new \DomainException("Valor de transferência deve ser maior que 0");
+        }
+
+        if ($this->origin_account->getTotalAccountMoney() < $this->value) {
+            throw new \DomainException('A conta de origem não possui o valor necessário para realizar a transferência.');
         }
     }
 }
